13f22a4ae4273452ffb3178e026de041a54c1cf3
StevenGuamanDev <stivenguaman88@gmail.com>
2026-07-18 19:45:58 -0500

    Actualización del portafolio y recursos

--- assets/js/enviomail.js
+++ assets/js/enviomail.js
@@ -6,7 +6,7 @@
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    emailjs.send("service_4h3rfyo","template_4i05fkg", {
+    emailjs.send("service_3nnh5nc","template_4i05fkg", {
         to_name: document.getElementById('to_name').value,
         user_email: document.getElementById('user_email').value,
         affair: document.getElementById('affair').value,
